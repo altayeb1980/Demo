6750b788a40e484ead75d8f9229f9bd07e8fea5f
Altayeb Saadeh <loay.saada@gmail.com>
2017-09-21 14:00:39 +0200

    add third change

--- Demo/src/com/epam/maven/Application.java
+++ Demo/src/com/epam/maven/Application.java
@@ -5,6 +5,7 @@ public class Application {
 	public static void main(String[] args) {
 		System.out.println("hello wrodl");
 		System.out.println("what a nice country");
+		System.out.println("what a poland people ya man");
 	}
 
 }  
